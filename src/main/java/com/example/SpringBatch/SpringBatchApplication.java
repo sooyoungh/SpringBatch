7ecac90f11bd5f53db5ed474a1efc5a30a52f616
sooyoungh <sooyoung.h8@gmail.com>
2023-05-24 12:06:56 +0900

    feat: Spring Batch 기본 구조

--- src/main/java/com/example/SpringBatch/SpringBatchApplication.java
+++ src/main/java/com/example/SpringBatch/SpringBatchApplication.java
@@ -1,8 +1,10 @@
 package com.example.SpringBatch;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableBatchProcessing // 이 설정이 있어줘야 배치 실행
 @SpringBootApplication
 public class SpringBatchApplication {
 
